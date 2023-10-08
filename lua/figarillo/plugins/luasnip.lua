9a3387e6ca8ac279ad80ae079da8627b3bcd1efd
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 06:09:02 -0300

    feat: add snip to lua snip

--- lua/figarillo/plugins/luasnip.lua
+++ lua/figarillo/plugins/luasnip.lua
@@ -3,4 +3,10 @@ return {
   keys = function()
     return {}
   end,
+  opts = function()
+    local luasnip = require("luasnip")
+    luasnip.filetype_extend("typescriptreact", { "javascript" })
+    luasnip.filetype_extend("typescript", { "javascript" })
+    require("luasnip/loaders/from_vscode").lazy_load()
+  end,
 }
