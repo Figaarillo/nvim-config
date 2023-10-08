d19966292f2d18ec23673fba33a0528a329d0d3b
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 04:00:54 -0300

    refactor: do not load bluloco plugin

--- lua/figarillo/plugins/bluloco.lua
+++ lua/figarillo/plugins/bluloco.lua
@@ -1,6 +1,10 @@
+if true then
+  return {}
+end
+
 return {
   "uloco/bluloco.nvim",
-  lazy = false,
+  lazy = true,
   priority = 1000,
   dependencies = { "rktjmp/lush.nvim" },
   opts = {
@@ -8,5 +12,6 @@ return {
     transparent = false,
     italics = true,
     terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
+    guicursor = true,
   },
 }
