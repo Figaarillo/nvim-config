a470badc7fd34c6a40bbc56f5fb10103812a3ee6
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 18:24:50 -0300

    refactor: remove codium cmp

--- lua/figarillo/plugins/init.lua
+++ lua/figarillo/plugins/init.lua
@@ -18,5 +18,4 @@ return {
   { import = "lazyvim.plugins.extras.test.core" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.vscode" },
-  { import = "lazyvim.plugins.extras.coding.codeium" },
 }
