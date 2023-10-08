a46d9132c86da0aa823b3df9be7eba6e1a3f6a6d
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 05:09:04 -0300

    feat: add new golang features in config

--- lua/figarillo/plugins/go.lua
+++ lua/figarillo/plugins/go.lua
@@ -47,6 +47,29 @@ return {
     },
   },
   {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      if type(opts.sources) == "table" then
+        local nls = require("null-ls")
+        vim.list_extend(opts.sources, {
+          nls.builtins.code_actions.gomodifytags,
+          nls.builtins.code_actions.impl,
+          nls.builtins.formatting.goimports,
+        })
+      end
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        go = { "goimports" },
+      },
+    },
+  },
+  {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = {
@@ -54,9 +77,35 @@ return {
         "mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          table.insert(opts.ensure_installed, "delve")
+          vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl", "goimports", "delve" })
         end,
       },
+      {
+        "leoluz/nvim-dap-go",
+        config = true,
+      },
     },
+  },
+  {
+    "leoluz/nvim-dap-go",
+    config = true,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-go",
+    },
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          -- Here we can set options for neotest-go, e.g.
+          -- args = { "-tags=integration" }
+        },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest-go",
   },
 }
