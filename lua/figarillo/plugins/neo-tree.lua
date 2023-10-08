747ae6a6c0f9222baa3ce51796e04c3333a12dba
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 19:25:34 -0300

    fix: fix neotree config

--- lua/figarillo/plugins/neo-tree.lua
+++ lua/figarillo/plugins/neo-tree.lua
@@ -36,7 +36,6 @@ return {
     open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
     filesystem = {
       bind_to_cwd = false,
-      follow_current_file = true,
       use_libuv_file_watcher = true,
     },
     window = {
