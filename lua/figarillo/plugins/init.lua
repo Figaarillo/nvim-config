3aec0647c997fe29a8f8cf1128f3e3acfc8bfabf
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 05:42:47 -0300

    feat: add support to python

--- lua/figarillo/plugins/init.lua
+++ lua/figarillo/plugins/init.lua
@@ -10,6 +10,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
