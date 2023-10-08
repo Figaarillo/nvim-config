3fb9bbaff0197b02f5533540581d2917db2807b0
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 05:16:38 -0300

    chore: update typescript config

--- lua/figarillo/plugins/typescript.lua
+++ lua/figarillo/plugins/typescript.lua
@@ -9,12 +9,26 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
     opts = {
       -- make sure mason installs the server
       servers = {
         ---@type lspconfig.options.tsserver
         tsserver = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+          },
           settings = {
             typescript = {
               format = {
@@ -35,20 +49,6 @@ return {
             },
           },
         },
-      },
-      setup = {
-        tsserver = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-            -- stylua: ignore
-            vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-            -- stylua: ignore
-            vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-          end)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
       },
     },
   },
