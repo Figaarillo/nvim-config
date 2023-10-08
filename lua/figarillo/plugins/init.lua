0286396e202aff6fc91d573f643a75b7d5dee204
Figaarillo <axel.leonardi.22@gmail.com>
2023-10-08 05:26:33 -0300

    feat: add support to c and c++

--- lua/figarillo/plugins/init.lua
+++ lua/figarillo/plugins/init.lua
@@ -9,6 +9,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
