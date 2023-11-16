fcac5f89d1b0072451ee1abd3307e22af60d9069
Figaarillo <axel.leonardi.22@gmail.com>
2023-11-16 00:23:06 -0300

    feat: add smoth cursor plugin

---
+++ lua/figarillo/plugins/smooth-cursor.lua
@@ -0,0 +1,12 @@
+return {
+  "gen740/SmoothCursor.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("smoothcursor").setup({
+      type = "exp",
+      fancy = {
+        enable = true,
+      },
+    })
+  end,
+}
