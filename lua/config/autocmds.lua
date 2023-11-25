0f271cc7e5c8760654d3e7c8d4f8cb94bba22f37
Figaarillo <axel.leonardi.22@gmail.com>
2023-11-25 01:15:44 -0300

    refactor: update neovide options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,16 +2,26 @@
 vim.loader.enable()
 
 -- ============================= Neovide ============================
--- @diagnostic disable: undefined-global
-vim.cmd([[
-	if exists("g:neovide")
-		set guifont=Operator\ Mono\ SSm\ Lig:h3.5
-    " set guifont=VictorMono\ Nerd\ NF\ Semi-Bold\ Italic:4
-		" set guifont=CaskaydiaCove\ Nerd\ Font\ Bold:h4
-		let g:neovide_scale_factor = 1
-		let g:neovide_cursor_vfx_mode = "pixiedust"
-		let g:neovide_remember_window_size = v:true
-    let g:neovide_transparency = 0.85
-    let g:neovide_background_color = '#0B0E14'
-	endif
-]])
+if vim.g.neovide then
+  vim.o.guifont = "Operator Mono Lig:h5.1:b"
+  -- vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h5.1"
+  -- vim.o.guifont = "VictorMono Nerd Font:h5.1:b"
+  vim.opt.linespace = 1
+  vim.g.neovide_scale_factor = 2
+  vim.g.neovide_transparency = 1
+  vim.g.neovide_background_color = "#0B0E14"
+  vim.g.neovide_refresh_rate = 60
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  vim.g.neovide_remember_window_size = true
+end
+
+-- ========================== Change font size =========================
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-=>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1 / 1.25)
+end)
