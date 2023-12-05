b394e843d17176e4408af434ed1fbaa020db2818
Figaarillo <axel.leonardi.22@gmail.com>
2023-12-05 20:01:09 -0300

    feat: add telescope customice config and keymaps

---
+++ lua/figarillo/plugins/telescope.lua
@@ -0,0 +1,96 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  keys = {
+    {
+      "<leader>fp",
+      function()
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+
+        require("telescope").extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
+      end,
+      desc = "File Explorer",
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_result = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+        },
+      },
+    })
+
+    opts.pickers = {
+      theme = "ivy",
+      diagnostics = {
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
+      },
+    }
+
+    opts.extensions = {
+      file_browser = {
+        theme = "dropdown",
+        hijack_netrw = true,
+        mappings = {
+          ["i"] = {
+            ["<C-w>"] = function()
+              vim.cmd("normal vbd")
+            end,
+          },
+          ["n"] = {
+            ["N"] = fb_actions.create,
+            ["h"] = fb_actions.goto_parent_dir,
+            ["/"] = function()
+              vim.cmd("startinsert")
+            end,
+            ["<C-u>"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_previous(prompt_bufnr)
+              end
+            end,
+            ["<C-d>"] = function(prompt_bufnr)
+              for i = 1, 10 do
+                actions.move_selection_next(prompt_bufnr)
+              end
+            end,
+            ["<PageUp>"] = actions.preview_scrolling_up,
+            ["<PageDown>"] = actions.preview_scrolling_down,
+          },
+        },
+      },
+    }
+    telescope.setup(opts)
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("file_browser")
+  end,
+}
