c82b83521eb02443e6537a88738d86f2ba460e78
Figaarillo <axel.leonardi.22@gmail.com>
2023-12-06 23:26:58 -0300

    feat: add keymap to enable and disable codeium

--- lua/figarillo/plugins/codeium.lua
+++ lua/figarillo/plugins/codeium.lua
@@ -1,7 +1,6 @@
 return {
-  {
-    "Exafunction/codeium.vim",
-    event = "InsertEnter",
+  "Exafunction/codeium.vim",
+  event = "InsertEnter",
     -- stylua: ignore
     config = function ()
       vim.g.codeium_disable_bindings = 1
@@ -12,5 +11,16 @@ return {
       vim.keymap.set("i", "<A-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
       vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
     end,
+  keys = {
+    {
+      "<leader>ccd",
+      "<cmd>CodeiumDisable<CR>",
+      desc = "Disable codeium",
+    },
+    {
+      "<leader>cce",
+      "<cmd>CodeiumEnable<CR>",
+      desc = "Enable codeium",
+    },
   },
 }
