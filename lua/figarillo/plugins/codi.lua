ea658a027e458cc8b4815d2fb809b3e819027ec1
Figaarillo <axel.leonardi.22@gmail.com>
2023-12-11 00:17:11 -0300

    feat: add REPL codi plugin

---
+++ lua/figarillo/plugins/codi.lua
@@ -0,0 +1,17 @@
+return {
+  "metakirby5/codi.vim",
+  event = "VeryLazy",
+  lazy = true,
+  keys = {
+    {
+      "<leader>ccc",
+      [[<cmd>Codi<CR>]],
+      desc = "Enable Codi",
+    },
+    {
+      "<leader>ccC",
+      [[<cmd>Codi!<CR>]],
+      desc = "Disable Codi",
+    },
+  },
+}
