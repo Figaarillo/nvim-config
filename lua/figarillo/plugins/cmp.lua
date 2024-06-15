f30b7c28689543dd1557b8a21f36501c3fb7b993
Figaarillo <axel.leonardi.22@gmail.com>
2024-06-15 10:45:20 -0300

    chore: remove max items in sources from cmp

--- lua/figarillo/plugins/cmp.lua
+++ lua/figarillo/plugins/cmp.lua
@@ -130,7 +130,7 @@ return {
         end,
       },
       sources = {
-        { name = "nvim_lsp", max_item_count = 10, keyword_length = 1 },
+        { name = "nvim_lsp" },
         { name = "luasnip", max_item_count = 10 },
         { name = "buffer", max_item_count = 5, keyword_length = 3 },
         { name = "emoji" },
