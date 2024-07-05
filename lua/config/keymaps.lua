b0402cf0e11303f69c04030411113a3bb4cb18bc
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-05 12:50:38 -0300

    refactor: enhance keymaps with default keybinds of LazyVim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,9 +16,8 @@ keymap({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<CR><esc>", { desc = "Save file" 
 keymap("n", "ZZZ", ":q!<CR>", opts) -- fast close
 keymap("n", "ZZ", "<cmd>Dashboard<CR>", opts) -- fast close
 keymap("n", "<M-z>", "<cmd>set wrap!<CR>", opts) -- toggle wrap
-keymap("i", "<C-BS>", "<C-W>", { desc = "Delete backward" }) -- Ctrl-Backspace to delete the previous word
--- keymap("n", "<C-a>", "gg<S-v>G", { desc = "Select all" }) -- Ctrl-a to select all
-keymap("n", "<C-c>", "<cmd>bd<CR>", { desc = "Close buffer" }) -- Ctrl-c to close buffer
+keymap("i", "<C-BS>", "<C-W>", { desc = "Delete backword" }) -- Ctrl-Backspace to delete the previous word
+keymap("n", "<C-c>", "<cmd>bd<CR>", { desc = "Delete Buffer and Window" }) -- Ctrl-c to close buffer
 
 -- =========================== Indent line ==========================
 keymap("v", ">", ">gv", opts)
@@ -32,19 +31,20 @@ keymap("n", "sv", "<cmd>vsplit<Return><C-w>w", opts)
 keymap("n", "+", "<C-a>", opts)
 keymap("n", "-", "<C-x>", opts)
 
--- ====================== Move line up and down =====================
-keymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-keymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- ========================= Better up/down =========================
+keymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+keymap({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+keymap({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- ======================== Resize the buffer =======================
-keymap("n", "<C-right>", "<C-w><")
-keymap("n", "<C-left>", "<C-w>>")
-keymap("n", "<C-up>", "<cmd>resize -1<CR>", opts)
-keymap("n", "<C-down>", "<cdm>resize +1<CR>", opts)
+keymap("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- ================== Moving within the insert mode =================
 keymap("i", "<C-h>", "<left>", opts)
-keymap("n", "-", "<C-x>", opts)
 keymap("i", "<C-j>", "<down>", opts)
 keymap("i", "<C-k>", "<up>", opts)
 keymap("i", "<C-l>", "<right>", opts)
@@ -56,13 +56,21 @@ keymap("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
 keymap("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
 keymap("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
 keymap("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
--- keymap("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
--- keymap("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+keymap("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+keymap("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+-- =========================== Move Lines ===========================
+keymap("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+keymap("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+keymap("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+keymap("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- ======================= Toggle sign column =======================
 keymap("n", "<leader>ua", "<cmd>set signcolumn=no<CR>", { desc = "Hide sign column" })
 keymap("n", "<leader>uA", "<cmd>set signcolumn=number<CR>", { desc = "Show sign column" })
 
 -- ======================== Multiple cursors ========================
-keymap("n", "<leader>j", "*``cgn", { desc = "Find next occurrence" })
 keymap("n", "<leader>k", "*``cgN", { desc = "Find previous occurrence" })
+keymap("n", "<leader>j", "*``cgn", { desc = "Find next occurrence" })
