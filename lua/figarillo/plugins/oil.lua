e3e066a134dd92c79f49bde1fe2de056ba57cb63
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-08 01:53:38 -0300

    chore: update oil config

--- lua/figarillo/plugins/oil.lua
+++ lua/figarillo/plugins/oil.lua
@@ -37,7 +37,7 @@ return {
     -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
     delete_to_trash = true,
     -- Skip the confirmation popup for simple operations
-    skip_confirm_for_simple_edits = false,
+    skip_confirm_for_simple_edits = true,
     -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
     prompt_save_on_select_new_entry = true,
     -- Oil will automatically delete hidden buffers after this delay
@@ -72,20 +72,21 @@ return {
     use_default_keymaps = true,
     view_options = {
       -- Show files and directories that start with "."
-      show_hidden = false,
+      show_hidden = true,
       -- This function defines what is considered a "hidden" file
       is_hidden_file = function(name, bufnr)
         return vim.startswith(name, ".")
       end,
       -- This function defines what will never be shown, even when `show_hidden` is set
       is_always_hidden = function(name, bufnr)
-        return false
+        return name == ".." or name == ".git"
       end,
-      sort = {
-        -- sort order can be "asc" or "desc"
-        -- see :help oil-columns to see which columns are sortable
-        { "type", "asc" },
-        { "name", "asc" },
+      -- sort = {
+      --   -- sort order can be "asc" or "desc"
+      --   -- see :help oil-columns to see which columns are sortable
+      --   { "type", "asc" },
+      --   { "name", "asc" },
+      natural_order = true
       },
     },
     -- Configuration for the floating window in oil.open_float
@@ -97,6 +98,7 @@ return {
       border = "rounded",
       win_options = {
         winblend = 0,
+        wrap = true,
       },
       -- This is the config that will be passed to nvim_open_win.
       -- Change values here to customize the layout
