0a926b841116aaccdc74cac30d247e8b9de43ea5
Figaarillo <axel.leonardi.22@gmail.com>
2024-07-21 11:20:59 -0300

    chore: remove cyberdream and github colorscheme

--- lua/figarillo/plugins/colorschemes.lua
+++ lua/figarillo/plugins/colorschemes.lua
@@ -53,17 +53,6 @@ return {
     },
   },
   {
-    "scottmckendry/cyberdream.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = {
-      italic_comments = true,
-      hide_fillchars = true,
-      borderless_telescope = true,
-      terminal_colors = true,
-    },
-  },
-  {
     "Mofiqul/dracula.nvim",
     priority = 1000, -- Ensure it loads first
     lazy = true,
@@ -223,13 +212,5 @@ return {
         },
       },
     },
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").compile()
-    end,
   },
 }
